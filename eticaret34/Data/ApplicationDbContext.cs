88e3b707709ea12185cddf230926a619fdc6b655
muratapk <muratapk@hotmail.com>
2023-09-27 21:23:42 +0300

    Add project files.

---
+++ eticaret34/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using eticaret34.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eticaret34.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
